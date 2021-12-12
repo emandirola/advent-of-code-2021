dbfb2985bbb6e3a5cf5de34669501dd719a4204d
Esteban Mandirola <esteban.mandirola@gmail.com>
2021-12-12 19:14:54 -0300

    day06 part02

--- src/Day06/Day06.hs
+++ src/Day06/Day06.hs
@@ -1,20 +1,34 @@
+{- LANGUAGE DeriveFunctor -}
+
+{-# LANGUAGE DeriveFunctor #-}
+
 module Day06.Day06 where
 
-import Data.List (foldl')
+import Data.List (foldl', sort, group)
+import Data.List.NonEmpty (groupAllWith)
 
+data Fish a = Fish { days :: Int, amount :: a }
+instance Num a => Semigroup (Fish a) where
+  (Fish d a1) <> (Fish _ a2) = Fish (d) (a1 + a2)
 
-part01 :: [Int] -> [Int]
-part01 fish = foldl' go fish [1..80]
+play :: Int -> [Int] -> Int
+play n fish = sum $ map amount $ foldl' go fish' [1..n]
   where
-    go :: [Int] -> Int -> [Int]
-    go fs _ = concat $ map go' fs
-      where
-        go' :: Int -> [Int]
-        go' f = if f > 0 then [f - 1] else [6, 8]
+    fish' = map (\x -> Fish (head x) (length x)) $ group $ sort fish
+    go fs _ = map (foldr1 (<>)) $ groupAllWith days $ concatMap go' fs
+    go' (Fish d a) = if d > 0 then [Fish (d-1) a] else [Fish 6 a, Fish 8 a]
+
+part01 :: [Int] -> Int
+part01 = play 80
+
+part02 :: [Int] -> Int
+part02 = play 256
 
 day06 :: IO ()
 day06 = do
   fish <- read <$> (\s -> "[" ++ s ++ "]") <$> readFile "input/day06.txt"
   putStrLn "Day 06"
   putStrLn "Part 1"
-  putStrLn $ show $ length $ part01 fish
+  putStrLn $ show $ part01 fish
+  putStrLn "Part 2"
+  putStrLn $ show $ part02 fish
