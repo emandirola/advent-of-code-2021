bc4865312500d5914292026611904a5d6e5b18e1
Esteban Mandirola <esteban.mandirola@gmail.com>
2021-12-19 18:00:51 -0300

    day10 part02

--- src/Day10/Day10.hs
+++ src/Day10/Day10.hs
@@ -1,28 +1,34 @@
 module Day10.Day10 where
 
-import Data.Text (Text, replace, pack, unpack, findIndex)
-import qualified Data.Text as T (length)
-import Data.Either (fromLeft, lefts)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Either (fromLeft, partitionEithers)
 import Data.Maybe (fromJust, isJust)
+import Data.List (sort)
 type Parser = (String, Maybe Char)
 
 chunks :: [Text]
-chunks = map pack ["()", "[]", "{}", "<>"]
-points :: Char -> Int
+chunks = map T.pack ["()", "[]", "{}", "<>"]
+ending :: [Char]
+ending = map ((!! 1) . T.unpack) chunks
+openingAndEnding :: [(Char, Char)]
+openingAndEnding = zip (map ((!! 0) . T.unpack) chunks) ending
+openingToEnding o = snd $ head $ filter (\(a, _) -> a == o) openingAndEnding
+points :: Char -> ((Int, Int), Char)
 points c = case c of
-  ')' -> 3
-  ']' -> 57
-  '}' -> 1197
-  '>' -> 25137
-  _ -> 0
+  ')' -> ((3, 1), '(')
+  ']' -> ((57, 2), '[')
+  '}' -> ((1197, 3), '{')
+  '>' -> ((25137, 4), '<')
+  _ -> ((0, 0), ' ')
 
 
-parse :: String -> Maybe Char
-parse s = fmap (\i -> unpack parsed !! i) $ findIndex (`elem` ending) $ parsed
+--parse :: String -> Maybe Char
+parse :: [Char] -> Text
+parse s = parsed
   where
-    parsed = fromLeft (pack "") $ parse' $ pack s
-    ending = map ((!! 1) . unpack) chunks
-    replaceAll = foldl1 (.) $ map (`replace` pack "") chunks
+    parsed = fromLeft (T.pack "") $ parse' $ T.pack s
+    replaceAll = foldl1 (.) $ map (`T.replace` T.pack "") chunks
     parse' :: Text -> Either Text Text
     parse' s' =
       let
@@ -31,10 +37,28 @@ parse s = fmap (\i -> unpack parsed !! i) $ findIndex (`elem` ending) $ parsed
         then Left replaced
         else parse' replaced
 
-part01 :: [String] -> Int
-part01 xs = sum $ map (points . fromJust) $ filter isJust xs'
+parseAll :: [String] -> ([Text], [Text])
+parseAll xs = partitionEithers xs'
   where
-    xs' = map parse xs
+    xs' = map parse' xs
+    parse' s = if T.any (`elem` ending) parsed
+      then Left parsed
+      else Right parsed
+      where
+        parsed = parse s
+
+part01 :: [String] -> Int
+part01 xs = sum $ map (fst . fst . points) lastChars
+  where
+    parsed = fst $ parseAll xs
+    lastChars = map fromJust $ filter isJust $ map (T.find (`elem` ending)) parsed
+
+part02 :: [String] -> Int
+part02 xs = parsed !! (length parsed `div` 2)
+  where
+    parsed = sort $ map (points' . T.unpack) $ snd $ parseAll xs
+    points' :: String -> Int
+    points' = foldl (\acc p -> acc * 5 + p) 0 . map (snd . fst . points . openingToEnding) . reverse
 
 input :: IO [String]
 input = lines <$> readFile "input/day10.txt"
@@ -45,4 +69,6 @@ day10 = do
   putStrLn "Day 10"
   putStrLn "Part 1"
   putStrLn $ show $ part01 input'
+  putStrLn "Part 2"
+  putStrLn $ show $ part02 input'
 
