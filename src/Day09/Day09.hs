74872db20159658d3f6f67ba1592b298457a2e54
Esteban Mandirola <esteban.mandirola@gmail.com>
2021-12-18 22:53:53 -0300

    day09 part01

--- src/Day09/Day09.hs
+++ src/Day09/Day09.hs
@@ -3,15 +3,11 @@
 
 module Day09.Day09 where
 
-import Data.List (intersperse)
-import Control.Monad (join)
-import Data.Maybe (isJust, fromJust)
-import Debug.Trace (trace)
-
 type Board = [Cell]
 type Position = (Int, Int)
+type Positions = [Position]
 type Cells = [Cell]
-data Cell = Cell Position Int Cells
+data Cell = Cell Position Int Positions deriving (Show, Eq)
 
 input :: IO [[Int]]
 input = map (map ((subtract (fromEnum '0')) . fromEnum)) . lines <$> readFile "input/Day09.example.txt"
@@ -19,43 +15,34 @@ input = map (map ((subtract (fromEnum '0')) . fromEnum)) . lines <$> readFile "i
 buildBoard :: [[Int]] -> Board
 buildBoard xss = board
   where
-    numbered = zip [0..] $ map (zip [0..]) xss
-    preboard = foldl (
-      \acc (y, xs) -> acc ++ map (
-        \(x, n) -> ((x, y), n)
-        ) xs
-      ) [] numbered
-    board = map (\((x, y), n) -> Cell (x, y) n (map lookupCell' $ adj x y)) preboard
-    lookupCell' = lookupCell board
+    positions = [(x, y) | y <- [0..length xss - 1], x <- [0..length (head xss) - 1]]
+    numbered = zip positions $ concat xss
+    board = map (
+        \(p, n) -> Cell p n (uncurry adj p)
+      ) numbered
     adj = adjacent (length $ head xss) (length xss)
-
-
 
 lookupCell :: Board -> Position -> Cell
 lookupCell board pos = filter (\(Cell p _ _) -> p == pos) board !! 0
 
 adjacent :: Int -> Int -> Int -> Int -> [Position]
-adjacent maxx maxy x y = filter (\(x', y') -> x' >= 0 && x' <= maxx && y' >= 0 && y' <= maxy) [
+adjacent maxx maxy x y = filter (\(x', y') -> x' >= 0 && x' < maxx && y' >= 0 && y' < maxy) [
     (x, y - 1),
     (x, y + 1),
     (x - 1, y),
     (x + 1, y)
   ]
---part01 :: [[Int]] -> Int
-part01 (xss :: [[Int]]) = sum $ map (+1) $ concat xss''
-  where
-    xss' :: [(Int, [(Int, Int)])]
-    xss' = zip [0..] $ map (zip [0..]) xss
-    xss'' = map (\(i, xs) -> map snd $ filter (\(j, x) -> cell x i j) xs) xss'
-    cell x i j = all ((>x) . fromJust) $ filter isJust [up, down, left, right]
-      where
-        up = join $ (j `lookup`) <$> ((i - 1) `lookup` xss')
-        down = join $ (j `lookup`) <$> ((i + 1) `lookup` xss')
-        left = join $ ((j - 1) `lookup`) <$> (i `lookup` xss')
-        right = join $ ((j +  1) `lookup`) <$> (i `lookup` xss')
 
+part01 :: Board -> Int
+part01 board = sum $ map ((+1) . (\(Cell _ n _) -> n)) $ board'
+  where
+    lookupCell' = lookupCell board
+    board' = filter (\(Cell _ n cs) -> all (\(Cell _ n' _) -> n < n') $ map lookupCell' cs) board
+
+part02 :: Board -> Int
+part02 = undefined
 
 day09 :: IO ()
 day09 = do
-  input' <- input
-  putStrLn $ show $ part01 input'
+  board <- buildBoard <$> input
+  putStrLn $ show $ part01 board
